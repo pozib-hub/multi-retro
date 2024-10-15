e645737c464421a59c757c5df00161af2e545ccd
pozib-hub <pozib@mail.ru>
2024-10-15 18:02:16 +0300

    feat: add git pull in install script

--- scripts/install.bat
+++ scripts/install.bat
@@ -4,6 +4,8 @@ mode con: cols=60 lines=40
 REM Автоматически определяем путь к корню
 cd /d "%~dp0.."
 
+git pull
+
 REM Создаем виртуальное окружение
 echo Creating a virtual environment
 echo loading...
